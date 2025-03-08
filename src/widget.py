423baeb63284e6b115054976fee4e0051e912918
Egor Farkov <fef84@icloud.com>
2025-03-09 01:00:58 +0800

    Добавлены тесты и исправления

--- src/widget.py
+++ src/widget.py
@@ -5,27 +5,35 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(all_name_card: str) -> str:
     """Функция обработки данных о карте"""
-    new_list = all_name_card.split()
-    card_num = []
-    card_alph = []
 
-    for i in new_list:
-        if i.isalpha():
-            card_alph.append(i)
+    try:
+        new_list = all_name_card.split()
+        card_num = []
+        card_alph = []
+
+        for i in new_list:
+            if i.isalpha():
+                card_alph.append(i)
+            else:
+                card_num.append(i)
+
+        name_letter_card = " ".join(card_alph)
+
+        if name_letter_card == "Счет":
+            name_number_card = get_mask_account("".join(card_num))
         else:
-            card_num.append(i)
+            name_number_card = get_mask_card_number("".join(card_num))
 
-    name_letter_card = " ".join(card_alph)
-
-    if name_letter_card == "Счет":
-        name_number_card = get_mask_account("".join(card_num))
-    else:
-        name_number_card = get_mask_card_number("".join(card_num))
-
-    return f"{name_letter_card} {name_number_card}"
+        return f"{name_letter_card} {name_number_card}"
+    except Exception as err:
+        raise ValueError(f"Error: {str(err)}")
 
 
 def get_date(my_date: str) -> str:
     """Функция обработки данных о счете"""
-    date_object = datetime.fromisoformat(my_date)
-    return date_object.strftime("%d.%m.%Y")
+
+    try:
+        date_object = datetime.fromisoformat(my_date)
+        return date_object.strftime("%d.%m.%Y")
+    except Exception:
+        raise ValueError("Invalid input ISO format datetime")
