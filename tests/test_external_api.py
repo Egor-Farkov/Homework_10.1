d4d411c3b753fed2545fe8487d088c891166dce0
Egor Farkov <fef84@icloud.com>
2025-03-16 02:55:56 +0800

    Реализована функция конвертации валют и тесты к ней.

---
+++ tests/test_external_api.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch
+
+from src.external_api import get_convert_currency
+
+
+@patch("requests.get")
+def test_get_convert_currency(mock_get):  # type: ignore
+    data_rub = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+    usd_amount = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560",
+    }
+
+    assert get_convert_currency(data_rub) == 31957.58
+
+    mock_get.return_value.json.return_value = {"result": 9.124}
+    assert get_convert_currency(usd_amount) == 9.124
