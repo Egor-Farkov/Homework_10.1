e2b004842229a6e1a050358d14484cb8ed1b0678
Egor Farkov <fef84@icloud.com>
2025-03-19 23:56:01 +0800

    Исправлены ошибки после проверки.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
+
 from src.external_api import get_convert_currency
 from src.utils import read_json
 
 if __name__ == "__main__":
-    read_j = read_json("/Users/egorfedorovic/Documents/Skypro/Home_works/H_w_skypro/data/operations.json")
+    read_j = read_json(os.path.abspath("data/operations.json"))
 
     for i in read_j:
         get_convert_currency(i)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import json
 import logging
+import os
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("/Users/egorfedorovic/Documents/Skypro/Home_works/H_w_skypro/logs/utils.log", "w")
+file_handler = logging.FileHandler(os.path.abspath("logs/utils.log"), "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s %(module)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
